5ee23c630619d9d42798c9aa14723af507db6bce
sivaskandha005 <88761026+sivaskandha005@users.noreply.github.com>
2026-04-29 23:05:54 +0530

    Delete vercel.js

--- vercel.js
+++
@@ -1,8 +0,0 @@
-{
-  "version": 2,
-  "rewrites": [
-    { "source": "/student/(.*)", "destination": "/api/index.js" },
-    { "source": "/summary", "destination": "/api/index.js" },
-    { "source": "/save-recording", "destination": "/api/index.js" }
-  ]
-}
